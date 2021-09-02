71ab510a49f91ee6d87e00d727ebe0faa884682f
KaiqueYudji <yudjikaique@gmail.com>
2021-09-02 17:25:23 -0300

    deu certoo uhu

--- reactjs/src/pages/monkchat/conteudo.js
+++ reactjs/src/pages/monkchat/conteudo.js
@@ -6,16 +6,31 @@ import LoadingBar from 'react-top-loading-bar'
 import { ContainerConteudo } from './conteudo.styled'
 import { ChatButton, ChatInput, ChatTextArea } from '../../components/outros/inputs'
 
+import Cookies from 'js-cookie'
 import { useState, useRef } from 'react';
+import { useHistory } from 'react-router-dom'
+
 
 import Api from '../../service/api';
 const api = new Api();
 
+function lerUsuarioLogado(navigation) {
+    let logado = Cookies.get('usuario-logado');
+    if (logado == null)
+    navigation.push('/');
+
+    let usuariologado = JSON.parse(logado);
+    return usuariologado;
+}
+
 
 export default function Conteudo() {
+    const navigation = useHistory();
+    let usuariologado =  lerUsuarioLogado(navigation);
+
     const [chat, setChat] = useState([]);
     const [sala, setSala] = useState('');
-    const [usu, setUsu] = useState('');
+    const [usu, setUsu] = useState(usuariologado.nm_usuario);
     const [msg, setMsg] = useState('')
 
     const loading = useRef(null);
@@ -40,7 +55,10 @@ export default function Conteudo() {
         loading.current.complete();
     }
 
-    const enviarMensagem = async () => {
+    const enviarMensagem = async (event) => {
+        if(event.type === "keypress" && (!event.ctrlKey || event.charCode == 13 ))
+        return;
+
         const resp = await api.inserirMensagem(sala, usu, msg);
         if (!validarResposta(resp)) 
             return;
@@ -66,6 +84,16 @@ export default function Conteudo() {
         toast.dark('💕 Sala cadastrada!');
         await carregarMensagens();
     }
+
+    const remover = async (id) =>{
+      const r = await api.removerMensagem(id);
+      if (!validarResposta(r)) 
+      return;
+  
+  toast.dark('💕 Mensagem removida!');
+  await carregarMensagens();
+
+    }
     
     return (
         <ContainerConteudo>
@@ -79,7 +107,7 @@ export default function Conteudo() {
                     </div>
                     <div>
                         <div className="label">Nick</div>
-                        <ChatInput value={usu} onChange={e => setUsu(e.target.value)} />
+                        <ChatInput value={usu}readOnly={true} />
                     </div>
                     <div>
                         <ChatButton onClick={inserirSala}> Criar </ChatButton>
@@ -88,7 +116,7 @@ export default function Conteudo() {
                 </div>
                 <div className="box-mensagem">
                     <div className="label">Mensagem</div>
-                    <ChatTextArea value={msg} onChange={e => setMsg(e.target.value)} />
+                    <ChatTextArea value={msg} onChange={e => setMsg(e.target.value)} onKeyPress={enviarMensagem} />
                     <ChatButton onClick={enviarMensagem} className="btn-enviar"> Enviar </ChatButton>
                 </div>
             </div>
@@ -103,6 +131,7 @@ export default function Conteudo() {
                     {chat.map(x =>
                         <div key={x.id_chat}>
                             <div className="chat-message">
+                                <div> <img onClick={ () => remover(x.id_chat)} src="/assets/images/lixeira.svg" alt="" style={{cursor:"pointer"}}/></div>
                                 <div>({new Date(x.dt_mensagem.replace('Z', '')).toLocaleTimeString()})</div>
                                 <div><b>{x.tb_usuario.nm_usuario}</b> fala para <b>Todos</b>:</div>
                                 <div> {x.ds_mensagem} </div>
